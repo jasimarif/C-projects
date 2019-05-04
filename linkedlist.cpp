df7a71fcf303766d8741971e4b289b5edc34dffa
jasimarif <48030880+jasimarif@users.noreply.github.com>
2019-05-04 20:23:36 +0500

    Add files via upload

---
+++ linkedlist.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+	int data;
+	node *next;
+};
+
+class linklist
+{
+	public:
+		
+	node *n;
+	node *t;
+	node *h;
+	
+
+linklist()
+{
+
+
+	
+	h=NULL;
+	
+}
+void addata(int x)
+{
+	node *n=new node;
+	t=n;
+	n->data= x;	
+
+	n->next = h;
+	h=n;
+	
+	
+}
+
+void display()
+{
+	node *current= h;
+while(current!=NULL)
+	{
+		
+		cout <<current->data <<endl;
+		cout <<current->next <<endl;
+	current= current->next;
+	}
+	
+	 
+}
+
+};
+int main()
+{ 
+	linklist l1;
+	char *c,u;
+	char v;
+	u='v';
+	
+	///l1.linklisted();
+	l1.addata(5);
+	l1.addata(10);
+	l1.display();
+}
